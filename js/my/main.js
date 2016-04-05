0c74e88afe6d1a9d013ab93a2c5951db46fd4c8a
loveyoghurt <xuchao33@wanda.cn>
2016-04-05 18:01:50 +0800

    Site updated: 2016-04-05 18:01:47

--- js/my/main.js
+++ js/my/main.js
@@ -1,20 +1,20 @@
 (function($){
   // Caption
-  var qrcode = new QRCode(document.getElementById("qrcode"), {
-  width : 100,
-  height : 100
-});
+//   var qrcode = new QRCode(document.getElementById("qrcode"), {
+//   width : 100,
+//   height : 100
+// });
   
-  $.ajax({
-  	url: 'http://dwz.cn/create.php',
-  	type: 'POST',
-  	data: {
-  		url: window.location.href
-  	},
-  	success: function(data){
-       console.log(data);
-       qrcode.makeCode(data);     
-  	}
-  })
+  // $.ajax({
+  // 	url: 'http://dwz.cn/create.php',
+  // 	type: 'POST',
+  // 	data: {
+  // 		url: window.location.href
+  // 	},
+  // 	success: function(data){
+  //      console.log(data);
+  //      qrcode.makeCode(data);     
+  // 	}
+  // })
   // qrcode.makeCode("http://loveyoghurt.xyz");
 })(jQuery);
